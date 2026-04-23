ffb5ac731db7fb65c1d01f92bf02277c45c7d42d
trikhaidev <trikhai.dev@gmail.com>
2026-04-23 22:13:04 +0700

    Ôn tập Task

---
+++ SomeThing/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace SomeThing;
+
+class Program
+{
+    static int count = 0;
+    static async Task<string> ExcuteSomeThing(CancellationToken cancellationToken)
+    {
+        do
+        {
+            System.Console.WriteLine("Count: " + count++);
+            await Task.Delay(1000, cancellationToken);
+        }while(!cancellationToken.IsCancellationRequested);
+        return "Done";
+    }
+
+    static async Task Main(string[] args)
+    {
+        var cts = new CancellationTokenSource();
+        //cts.CancelAfter(5000);
+        var t1 = ExcuteSomeThing(cts.Token);
+
+        var ta = new Task<string>((title) =>
+        {
+            while (true)
+            {
+                System.Console.WriteLine(title);
+                Thread.Sleep(1000);
+            }
+            return "Done Task";
+        },"Hello something", cts.Token);
+        ta.Start();
+
+        Console.ReadLine();
+        System.Console.WriteLine("Canceling...");
+        cts.Cancel();
+        System.Console.WriteLine("Canceled");
+        Console.ReadLine();
+    }
+}
