f0fff457dbc46b262ae5bec0eb9631c22a090d8c
trikhaidev <trikhai.dev@gmail.com>
2026-07-02 23:03:51 +0700

    Triển khai phân quyền controller WeatherForecast

--- JwtAuth/Controllers/WeatherForecastController.cs
+++ JwtAuth/Controllers/WeatherForecastController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JwtAuth.Controllers;
@@ -12,6 +13,7 @@ public class WeatherForecastController : ControllerBase
     ];
 
     [HttpGet(Name = "GetWeatherForecast")]
+    [Authorize(Roles = "Admin,User")]
     public IEnumerable<WeatherForecast> Get()
     {
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
